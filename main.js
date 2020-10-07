d225b9250f506334367266a085532111ebbe8af9
Alex Despi <ajdespi2277@gmail.com>
2020-10-07 10:16:15 +0800

    media section

--- main.js
+++ main.js
@@ -9,23 +9,19 @@ hamburger.addEventListener("click", () => {
   mobile_menu.classList.toggle("active");
 });
 
-
-
-
-
 // cursor
 let mouseCursor = document.querySelector(".cursor");
+let mouseCursor2 = document.querySelector(".cursor2");
 let allLinks = document.querySelectorAll(".links");
 
 window.addEventListener("mousemove", cursor);
 
 function cursor(e) {
-  mouseCursor.style.top = e.pageY + "px ";
-  mouseCursor.style.left = e.pageX + "px";
-  // console.log(e);
+  mouseCursor.style.top = mouseCursor2.style.top = e.pageY + "px ";
+  mouseCursor.style.left = mouseCursor2.style.left = e.pageX + "px";
 }
 
-allLinks.forEach(link => {
+allLinks.forEach((link) => {
   link.addEventListener("mouseleave", () => {
     mouseCursor.classList.remove("link-grow");
     link.classList.remove("hovered-link");
@@ -37,66 +33,6 @@ allLinks.forEach(link => {
 });
 
 
-// // type writer
-
-// const TypeWriter = function (txtElement, words, wait = 1000) {
-//   this.txtElement = txtElement;
-//   this.words = words;
-//   this.txt = "";
-//   this.wordIndex = 0;
-//   this.wait = parseInt(wait, 20);
-//   this.type();
-//   this.isDeleting = false;
-// };
-
-// // type method
-// TypeWriter.prototype.type = function () {
-//   const current = this.wordIndex % this.words.length;
-//   const fullTxt = this.words[current];
-
-//   //   console.log(fullTxt);
-//   // check if deleting
-//   if (this.isDeleting) {
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
-
-//   // insert text into element
-//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-//   // init type speed
-//   let typeSpeed = 120;
-
-//   if (this.isDeleting) {
-//     typeSpeed /= 2;
-//   }
-
-//   if (!this.isDeleting && this.txt === fullTxt) {
-//     typeSpeed = this.wait;
-//     this.isDeleting = true;
-//   } else if (this.isDeleting && this.txt === "") {
-//     this.isDeleting = false;
-//     this.wordIndex++;
-
-//     typeSpeed = 300;
-//   }
-
-//   setTimeout(() => this.type(), typeSpeed);
-// };
-
-// //  Init on DOM Load
-// document.addEventListener("DOMContentLoaded", init);
-
-// // init App
-// function init() {
-//   const txtElement = document.querySelector(".txt-type");
-//   const words = JSON.parse(txtElement.getAttribute("data-words"));
-//   const wait = txtElement.getAttribute("data-wait");
-//   // init Typewriter
-//   new TypeWriter(txtElement, words, wait);
-// }
-//  end of type writer
 
 var app = document.getElementById("app");
 
